f38fad669af7c8f87f87ba6de4e35dacf0eba171
Filip Curkovic <filip.curkovic1@gmail.com>
2020-03-26 20:56:53 +0100

    zupanije hardcoded

--- src/MojPosaoScrapper.java
+++ src/MojPosaoScrapper.java
@@ -5,12 +5,8 @@ import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 import org.jsoup.select.NodeVisitor;
 
-import java.io.IOException;
-import java.text.Normalizer;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MojPosaoScrapper extends WebScrapper implements Scrapper {
     private final String URL = "https://www.moj-posao.net/Pretraga-Poslova/";
@@ -22,9 +18,6 @@ public class MojPosaoScrapper extends WebScrapper implements Scrapper {
         try {
             Element current = null;
             do {
-                if (Provinces.getInstance().getProvincesMap() == null) {
-                    Provinces.getInstance().setProvincesMap(this.getProvinces());
-                }
 
                 Document doc = Jsoup.connect(this.buildURL(searchConfig)).get();
                 Element searchList = doc.getElementsByClass("searchlist").first();
@@ -46,12 +39,12 @@ public class MojPosaoScrapper extends WebScrapper implements Scrapper {
     }
 
     private String buildURL(SearchConfig searchConfig) {
-        String areaKey = Provinces.getInstance().getProvincesMap().get(searchConfig.province);
+//        String areaKey = Provinces.getInstance().getProvincesMap().get(searchConfig.province);
         System.out.println();
         return this.URL +
                 "?searchWord=" + searchConfig.profession +
                 "&keyword=" + searchConfig.profession +
-                "&job_title=&job_title_id=&area=" + (areaKey == null ? "" : areaKey) +
+//                "&job_title=&job_title_id=&area=" + (areaKey == null ? "" : areaKey) +
                 "&category=&page=" + this.currPage;
     }
 
@@ -95,35 +88,5 @@ public class MojPosaoScrapper extends WebScrapper implements Scrapper {
                 tempList.get(4),
                 tempList.get(2)
         );
-    }
-
-    private Map<String, String> getProvinces() throws IOException {
-        Document doc = Jsoup.connect(this.URL).get();
-        Map<String, String> provinceMap = new HashMap<>();
-        Element select = doc.getElementById("form_mainsearch_area");
-
-        select.children().traverse(new NodeVisitor() {
-            @Override
-            public void head(Node node, int i) {
-                if (node instanceof Element) {
-                    String province_HR = ((Element) node).text().toLowerCase();
-                    province_HR = Normalizer.normalize(province_HR, Normalizer.Form.NFD);
-                    province_HR = province_HR.replaceAll("[^\\p{ASCII}]", "");
-                    provinceMap.put(
-                            province_HR,
-                            node.attr("value")
-                    );
-                    System.out.println(province_HR);
-                    System.out.println(node.attr("value"));
-                }
-            }
-
-            @Override
-            public void tail(Node node, int i) {
-
-            }
-        });
-        provinceMap.put("", "");
-        return provinceMap;
     }
 }
